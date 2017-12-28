56f126bec48f6310ee72b36f9b534bc1e9db8b25
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-12-28 22:47:10 +0300

    BasicUnitAI: fix crash where there is only one unit on map.

--- sources/CastlesStrategyServer/Unit/BasicUnitAi.cpp
+++ sources/CastlesStrategyServer/Unit/BasicUnitAi.cpp
@@ -1,6 +1,8 @@
 #include "BasicUnitAi.hpp"
 #include <Urho3D/Scene/Node.h>
 #include <Urho3D/Scene/Scene.h>
+
+#include <Urho3D/IO/Log.h>
 #include <Urho3D/Navigation/NavigationMesh.h>
 #include <CastlesStrategyServer/Managers/UnitsManager.hpp>
 
@@ -9,13 +11,14 @@ namespace CastlesStrategy
 UnitCommand BasicUnitAI (Unit *self, const UnitType &unitType, UnitsManager *unitsManager)
 {
     Unit *nearestEnemy = unitsManager->GetNearestEnemy (self);
-    float distance = (self->GetNode ()->GetWorldPosition () - nearestEnemy->GetNode ()->GetWorldPosition ()).Length ();
+    float distance = nearestEnemy != nullptr ?
+                     (self->GetNode ()->GetWorldPosition () - nearestEnemy->GetNode ()->GetWorldPosition ()).Length () : 0.0f;
 
-    if (distance <= unitType.GetAttackRange ())
+    if (nearestEnemy != nullptr && distance <= unitType.GetAttackRange ())
     {
         return {UCT_ATTACK_UNIT, nearestEnemy->GetID ()};
     }
-    else if (distance <= unitType.GetVisionRange ())
+    else if (nearestEnemy != nullptr && distance <= unitType.GetVisionRange ())
     {
         return {UCT_FOLLOW_UNIT, nearestEnemy->GetID ()};
     }
@@ -30,6 +33,7 @@ UnitCommand BasicUnitAI (Unit *self, const UnitType &unitType, UnitsManager *uni
 
         distance = (self->GetNode ()->GetWorldPosition () - target).Length ();
         unsigned nextWaypoint = self->GetCurrentWaypointIndex () + 1;
+
 
         if (distance < unitType.GetAttackRange () && nextWaypoint < self->GetWaypoints ().Size ())
         {
