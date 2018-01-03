d14180719912aafdaa739abae1e2c7b70470c6e7
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-01-03 23:02:07 +0300

    Add Player class.

---
+++ sources/CastlesStrategyServer/Player/Player.cpp
@@ -0,0 +1,127 @@
+#include "Player.hpp"
+#include <CastlesStrategyServer/Managers/UnitsManager.hpp>
+#include <ActivitiesApplication/UniversalException.hpp>
+
+namespace CastlesStrategy
+{
+Player::Player (const ManagersHub *managersHub) :
+    managersHub_ (managersHub),
+    coins_ (0),
+    name_ (),
+    belongingMaterialIndex_ (0),
+
+    orders_ (),
+    unitsPull_ (dynamic_cast <const UnitsManager *> (managersHub->GetManager (MI_UNITS_MANAGER))->GetUnitsTypesCount ())
+{
+
+}
+
+Player::~Player ()
+{
+
+}
+
+void Player::HandleUpdate (float timeStep)
+{
+    if (!orders_.Empty ())
+    {
+        RecruitmentOrder &order = orders_.Front ();
+        order.timeLeft_ -= timeStep;
+
+        if (order.timeLeft_ <= 0.0f)
+        {
+            unitsPull_[order.unitType_]++;
+            orders_.PopFront ();
+        }
+    }
+}
+
+unsigned int Player::GetCoins () const
+{
+    return coins_;
+}
+
+void Player::SetCoins (unsigned int coins)
+{
+    coins_ = coins;
+}
+
+const Urho3D::String &Player::GetName () const
+{
+    return name_;
+}
+
+void Player::SetName (const Urho3D::String &name)
+{
+    name_ = name;
+}
+
+unsigned int Player::GetBelongingMaterialIndex () const
+{
+    return belongingMaterialIndex_;
+}
+
+void Player::SetBelongingMaterialIndex (unsigned int belongingMaterialIndex)
+{
+    belongingMaterialIndex_ = belongingMaterialIndex;
+}
+
+const Urho3D::List <RecruitmentOrder> &Player::GetOrders () const
+{
+    return orders_;
+}
+
+void Player::AddOrder (unsigned int unitType)
+{
+    const UnitsManager *unitsManager = dynamic_cast <const UnitsManager *> (managersHub_->GetManager (MI_UNITS_MANAGER));
+    const UnitType &unitTypeData = unitsManager->GetUnitType (unitType);
+
+    if (coins_ >= unitTypeData.GetRecruitmentCost ())
+    {
+        coins_ -= unitTypeData.GetRecruitmentCost ();
+        orders_.Push ({unitType, unitTypeData.GetRecruitmentTime ()});
+    }
+}
+
+void Player::RemoveOrder (unsigned int unitType)
+{
+    if (orders_.Empty ())
+    {
+        return;
+    }
+
+    for (auto iterator = ++orders_.Begin (); iterator != orders_.End (); iterator++)
+    {
+        if (iterator->unitType_ == unitType)
+        {
+            iterator = orders_.Erase (iterator);
+            return;
+        }
+    }
+
+    if (orders_.Front ().unitType_ == unitType)
+    {
+        orders_.PopFront ();
+    }
+}
+
+void Player::TakeUnitFromPull (unsigned int unitType)
+{
+    if (GetUnitsPullCount (unitType) == 0)
+    {
+        throw UniversalException <UnitsManager> ("Player: unit with type " + Urho3D::String (unitType) +
+                                                 " requested from pull, but there is no units of this type in the pull!");
+    }
+    unitsPull_ [unitType]--;
+}
+
+unsigned int Player::GetUnitsPullCount (unsigned int unitType) const
+{
+    if (unitType >= unitsPull_.Size ())
+    {
+        throw UniversalException <UnitsManager> ("Player: unit type " + Urho3D::String (unitType) +
+                                                 " requested, but there is only " + Urho3D::String (unitsPull_.Size ()) + " unit types!");
+    }
+    return unitsPull_ [unitType];
+}
+}
---
+++ sources/CastlesStrategyServer/Player/Player.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <Urho3D/Container/Vector.h>
+#include <Urho3D/Container/List.h>
+#include <Urho3D/Container/Str.h>
+#include <CastlesStrategyServer/Managers/ManagersHub.hpp>
+
+namespace CastlesStrategy
+{
+struct RecruitmentOrder
+{
+    unsigned int unitType_;
+    float timeLeft_;
+};
+
+class Player
+{
+public:
+    Player (const ManagersHub *managersHub);
+    virtual ~Player ();
+
+    void HandleUpdate (float timeStep);
+    unsigned int GetCoins () const;
+    void SetCoins (unsigned int coins);
+
+    const Urho3D::String &GetName () const;
+    void SetName (const Urho3D::String &name);
+
+    unsigned int GetBelongingMaterialIndex () const;
+    void SetBelongingMaterialIndex (unsigned int belongingMaterialIndex);
+
+    const Urho3D::List <RecruitmentOrder> &GetOrders () const;
+    void AddOrder (unsigned int unitType);
+    void RemoveOrder (unsigned int unitType);
+
+    void TakeUnitFromPull (unsigned int unitType);
+    unsigned int GetUnitsPullCount (unsigned int unitType) const;
+
+private:
+    const ManagersHub *managersHub_;
+    unsigned int coins_;
+    Urho3D::String name_;
+    unsigned int belongingMaterialIndex_;
+
+    Urho3D::List <RecruitmentOrder> orders_;
+    Urho3D::Vector <unsigned int> unitsPull_;
+};
+}
