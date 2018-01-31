5ad366f29933c4badcc3f2004cdbebcc8582d0fc
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-01-31 20:45:47 +0300

    Add scaffold for IngameActivity.

---
+++ sources/CastlesStrategy/Client/Ingame/IngameActivity.cpp
@@ -0,0 +1,119 @@
+#include "IngameActivity.hpp"
+#include <Urho3D/Core/Context.h>
+#include <Urho3D/Network/Network.h>
+#include <Urho3D/Network/NetworkEvents.h>
+
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Engine/Application.h>
+
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/Viewport.h>
+#include <Urho3D/Graphics/Camera.h>
+
+#include <Utils/UIResizer.hpp>
+#include <CastlesStrategy/Shared/Network/ServerConstants.hpp>
+
+namespace CastlesStrategy
+{
+IngameActivity::IngameActivity (Urho3D::Context *context, const Urho3D::String &playerName, const Urho3D::String &serverAddress,
+                                unsigned int port)
+        : ActivitiesApplication::Activity (context),
+          playerName_ (playerName),
+          serverAddress_ (serverAddress),
+          port_ (port),
+          scene_ (new Urho3D::Scene (context))
+{
+    InitScene ();
+}
+
+IngameActivity::~IngameActivity ()
+{
+    delete scene_;
+}
+
+void IngameActivity::Start ()
+{
+    SubscribeToEvents ();
+    ConnectToServer ();
+    SetupViewport ();
+}
+
+void IngameActivity::Update (float timeStep)
+{
+
+}
+
+void IngameActivity::Stop ()
+{
+
+}
+
+const Urho3D::String &IngameActivity::GetServerAddress () const
+{
+    return serverAddress_;
+}
+
+unsigned int IngameActivity::GetPort () const
+{
+    return port_;
+}
+
+const Urho3D::Scene *IngameActivity::GetScene () const
+{
+    return scene_;
+}
+
+void IngameActivity::InitScene () const
+{
+    UIResizer *uiResizer = scene_->CreateComponent <UIResizer> (Urho3D::LOCAL);
+    scene_->CreateComponent <Urho3D::Octree> (Urho3D::LOCAL);
+    Urho3D::Camera *camera = scene_->CreateChild ("Camera", Urho3D::REPLICATED)->
+            CreateComponent <Urho3D::Camera> (Urho3D::LOCAL);
+}
+
+void IngameActivity::SubscribeToEvents ()
+{
+    SubscribeToEvent (Urho3D::E_CONNECTFAILED, URHO3D_HANDLER (IngameActivity, HandleConnectFailed));
+    SubscribeToEvent (Urho3D::E_SERVERCONNECTED, URHO3D_HANDLER (IngameActivity, HandleServerConnected));
+    SubscribeToEvent (Urho3D::E_SERVERDISCONNECTED, URHO3D_HANDLER (IngameActivity, HandleServerDisconnected));
+    SubscribeToEvent (Urho3D::E_NETWORKMESSAGE, URHO3D_HANDLER (IngameActivity, HandleNetworkMessage));
+}
+
+void IngameActivity::ConnectToServer () const
+{
+    Urho3D::VariantMap identity;
+    identity [IdentityFields::NAME] = playerName_;
+
+    Urho3D::Network *network = context_->GetSubsystem <Urho3D::Network> ();
+    network->Connect (serverAddress_, port_, scene_, identity);
+}
+
+void IngameActivity::SetupViewport () const
+{
+    Urho3D::Renderer *renderer = context_->GetSubsystem <Urho3D::Renderer> ();
+    renderer->SetNumViewports (1);
+    renderer->SetViewport (0, new Urho3D::Viewport (
+            context_, scene_, scene_->GetChild ("Camera")->GetComponent <Urho3D::Camera> ()));
+}
+
+void IngameActivity::HandleConnectFailed (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData)
+{
+
+}
+
+void IngameActivity::HandleServerConnected (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData)
+{
+
+}
+
+void IngameActivity::HandleServerDisconnected (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData)
+{
+
+}
+
+void IngameActivity::HandleNetworkMessage (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData)
+{
+
+}
+}
---
+++ sources/CastlesStrategy/Client/Ingame/IngameActivity.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <ActivitiesApplication/Activity.hpp>
+#include <Urho3D/Scene/Scene.h>
+
+namespace CastlesStrategy
+{
+class IngameActivity : public ActivitiesApplication::Activity
+{
+URHO3D_OBJECT (IngameActivity, Activity)
+public:
+    IngameActivity (Urho3D::Context *context, const Urho3D::String &playerName, const Urho3D::String &serverAddress,
+                        unsigned int port);
+    virtual ~IngameActivity ();
+
+    virtual void Start ();
+    virtual void Update (float timeStep);
+    virtual void Stop ();
+
+    const Urho3D::String &GetServerAddress () const;
+    unsigned int GetPort () const;
+    const Urho3D::Scene *GetScene () const;
+
+private:
+    void InitScene () const;
+    void SubscribeToEvents ();
+    void ConnectToServer () const;
+    void SetupViewport () const;
+
+    void HandleConnectFailed (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData);
+    void HandleServerConnected (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData);
+    void HandleServerDisconnected (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData);
+    void HandleNetworkMessage (Urho3D::StringHash eventType, Urho3D::VariantMap &eventData);
+
+    Urho3D::String playerName_;
+    Urho3D::String serverAddress_;
+    unsigned int port_;
+    Urho3D::Scene *scene_;
+};
+}
+
