c0c55030c99afdd0e8e77112fb604160f7c2c0fc
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-03-31 22:50:27 +0300

    DataManager: add players map.

--- sources/CastlesStrategy/Client/Ingame/DataManager.hpp
+++ sources/CastlesStrategy/Client/Ingame/DataManager.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <Urho3D/Core/Context.h>
 #include <CastlesStrategy/Shared/Unit/UnitType.hpp>
+#include <CastlesStrategy/Shared/PlayerType.hpp>
 
 namespace CastlesStrategy
 {
@@ -10,6 +11,12 @@ struct RecruitmentOrder
 {
     unsigned int unitType_;
     float timeLeft_;
+};
+
+struct PlayerData
+{
+    PlayerType playerType_;
+    bool readyForStart_;
 };
 
 class DataManager : public Urho3D::Object
@@ -43,6 +50,12 @@ public:
     void SetSelectedSpawnNode (Urho3D::Node *selectedSpawnNode);
     unsigned int GetPredictedOrdedUnitsCount (unsigned int unitType) const;
 
+    void AddPlayer (const Urho3D::String &name, PlayerType playerType, bool readyForStart);
+    void RemovePlayer (const Urho3D::String &name);
+    void SetPlayerType (const Urho3D::String &name, PlayerType playerType);
+    void SetIsPlayerReadyForStart (const Urho3D::String &name, bool readyForStart);
+    const Urho3D::HashMap <Urho3D::String, PlayerData> &GetPlayers ();
+
 private:
     void AttemptToAddPrefabs ();
     void PredictOrders (float timeStep);
@@ -58,5 +71,6 @@ private:
 
     Urho3D::Vector <RecruitmentOrder> predictedOrders_;
     Urho3D::Vector <unsigned int> predictedOrderedUnitsCounts_;
+    Urho3D::HashMap <Urho3D::String, PlayerData> players_;
 };
 }
